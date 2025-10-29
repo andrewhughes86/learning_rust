8cf1b7f6c8a4a45d9f66c754757d8fd5cfbc71da
Andrew Hughes <andrewhughes86@gmail.com>
2025-10-28 21:33:24 -0600

    Added echo clone project

---
+++ echor/tests/cli.rs
@@ -0,0 +1,18 @@
+use assert_cmd::Command;
+use std::fs;
+
+#[test]
+fn dies_no_args() {
+    let mut cmd = Command::cargo_bin("echor").unwrap();
+    cmd.assert()
+        .failure()
+        .stderr(predicates::str::contains("Useage"));
+}
+
+#[test]
+fn hello() {
+    let outfile = "test/expected/hello1.txt";
+    let expected = fs::read_to_string(outfile).unwrap();
+    let mut cmd = Command::cargo_bin("echor").unwrap();
+    cmd.arg("Hello there").assert().success().stdout(expected);
+}
